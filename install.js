61b94f11b2d56761e396b4f4d87cea4c2b1b9b52
Chris Grimmett <chris@grimtech.net>
2018-10-05 03:40:09 -0700

    fixes for iss1

--- install.js
+++ install.js
@@ -14,20 +14,26 @@ const isWin = require('is-windows')();
   const {stdout: version} = await execa(binPath, ['--version'])
 
   // create path file
-  await fs.writeFile(path.join(__dirname, 'bin-path.js'), `module.exports = "${normalize(binPath)}"`)
-
-  console.log(`Installed successfully. (ver ${version})`)
+  try {
+    await fs.writeFile(path.join(__dirname, 'bin-path.js'), `module.exports = "${normalize(binPath)}"`)
+    console.log(`Installed successfully. (ver ${version})`)
+  } catch (err) {
+    console.error(err)
+  }
 })()
 
 async function install () {
   try {
     if (await commandExists('youtube-dl')) return 'youtube-dl'
+
+    const filename = isWin ? 'youtube-dl.exe' : 'youtube-dl'
+    const dirPath = path.join(__dirname, 'bin')
+
+    await download('https://yt-dl.org/downloads/latest/' + filename, dirPath)
+    await fs.chmod(path.join(dirPath, filename), 0o755)
   } catch (err) {
+    console.error(err)
   }
 
-  const filename = isWin ? 'youtube-dl.exe' : 'youtube-dl'
-  const dirPath = path.join(__dirname, 'bin')
-
-  await download('https://yt-dl.org/downloads/latest/' + filename, dirPath)
   return path.join(dirPath, filename)
 }
