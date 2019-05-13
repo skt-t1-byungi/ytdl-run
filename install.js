bf7eccbeb37974cc0eb22e6a2d3017594c5b5a9d
skt-t1-byungi <tiniwz@gmail.com>
2019-05-13 21:41:12 +0900

    fix remove renovate, update pkg, lint

--- install.js
+++ install.js
@@ -8,26 +8,26 @@ const normalize = require('normalize-path')
 const isWin = require('is-windows')();
 
 (async _ => {
-  console.log('installing youtube-dl...')
+    console.log('installing youtube-dl...')
 
-  const binPath = await install()
-  const {stdout: version} = await execa(binPath, ['--version'])
+    const binPath = await install()
+    const {stdout: version} = await execa(binPath, ['--version'])
 
-  // create path file
-  await fs.writeFile(path.join(__dirname, 'bin-path.js'), `module.exports = "${normalize(binPath)}"`)
+    // create path file
+    await fs.writeFile(path.join(__dirname, 'bin-path.js'), `module.exports = "${normalize(binPath)}"`)
 
-  console.log(`Installed successfully. (ver ${version})`)
+    console.log(`Installed successfully. (ver ${version})`)
 })()
 
 async function install () {
-  if (commandExists.sync('youtube-dl')) return 'youtube-dl'
+    if (commandExists.sync('youtube-dl')) return 'youtube-dl'
 
-  const filename = isWin ? 'youtube-dl.exe' : 'youtube-dl'
-  const dirPath = path.join(__dirname, 'bin')
+    const filename = isWin ? 'youtube-dl.exe' : 'youtube-dl'
+    const dirPath = path.join(__dirname, 'bin')
 
-  await download('https://yt-dl.org/downloads/latest/' + filename, dirPath)
-  const binPath = path.join(dirPath, filename)
-  await fs.chmod(binPath, 0o755)
+    await download('https://yt-dl.org/downloads/latest/' + filename, dirPath)
+    const binPath = path.join(dirPath, filename)
+    await fs.chmod(binPath, 0o755)
 
-  return binPath
+    return binPath
 }
--- test.js
+++ test.js
@@ -7,32 +7,32 @@ const testVideo = 'https://www.youtube.com/watch?v=C0DPdy98e4c'
 const tmpDir = path.join(__dirname, 'tmp/')
 
 test.before(async t => {
-  await fs.ensureDir(tmpDir)
+    await fs.ensureDir(tmpDir)
 })
 
 test.after(async t => {
-  await fs.remove(tmpDir)
+    await fs.remove(tmpDir)
 })
 
 test('basic, stream', async t => {
-  await ytdl([testVideo, '-f', 'mp4', '-o', 'basic.mp4'], {cwd: tmpDir})
-  const {size} = await fs.stat(path.join(tmpDir, 'basic.mp4'))
-  t.truthy(size)
+    await ytdl([testVideo, '-f', 'mp4', '-o', 'basic.mp4'], {cwd: tmpDir})
+    const {size} = await fs.stat(path.join(tmpDir, 'basic.mp4'))
+    t.truthy(size)
 
-  const streamPath = path.join(tmpDir, 'stream.mp4')
-  await new Promise(
-    (resolve, reject) => ytdl.stream([testVideo, '-f', 'mp4'], {cwd: tmpDir})
-      .stdout
-      .pipe(fs.createWriteStream(streamPath))
-      .on('finish', resolve)
-      .on('error', reject)
-  )
+    const streamPath = path.join(tmpDir, 'stream.mp4')
+    await new Promise(
+        (resolve, reject) => ytdl.stream([testVideo, '-f', 'mp4'], {cwd: tmpDir})
+            .stdout
+            .pipe(fs.createWriteStream(streamPath))
+            .on('finish', resolve)
+            .on('error', reject)
+    )
 
-  const {size: streamSize} = await fs.stat(streamPath)
-  t.is(streamSize, size)
+    const {size: streamSize} = await fs.stat(streamPath)
+    t.is(streamSize, size)
 })
 
 test('getInfo', async t => {
-  const {title} = await ytdl.getInfo(testVideo)
-  t.is(title, 'TEST VIDEO')
+    const {title} = await ytdl.getInfo(testVideo)
+    t.is(title, 'TEST VIDEO')
 })
